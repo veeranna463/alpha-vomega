d8e34761e593261852c76e10f72c889f3d3e5ade
veeranna463 <makkella.veeranna@gmail.com>
2025-07-05 10:07:37 +0530

    update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -41,10 +41,11 @@ function Footer() {
         <div className="footer-socials">
           <h3>Follow Us</h3>
           <div className="social-icons">
-            {/* <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+           {/* <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
 <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
 <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a> */}
+
 
           </div>
         </div>
